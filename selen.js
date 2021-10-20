9e5f5a93766177f45be55325544f59351476e5a3
selenozcan <ozcsel.16@gmail.com>
2021-10-21 00:14:46 +0900

    timeline added

---
+++ selen.js
@@ -0,0 +1,46 @@
+$(function () {
+    $("#button-timeline").click(function () {
+      setTimeout(function() {
+        $(".timeline").fadeIn();}, 500);
+      $(".home").fadeOut();
+      $(".skills").fadeOut();
+      $(".projects").fadeOut();
+      $(".cv").fadeOut();
+      
+    });
+    $("#button-skills").click(function () { 
+      setTimeout(function() {
+        $(".skills").fadeIn();}, 500);
+      $(".home").fadeOut();
+      $(".timeline").fadeOut();
+      $(".projects").fadeOut();
+      $(".cv").fadeOut();
+      
+    });
+    $("#button-projects").click(function () {
+      setTimeout(function() {
+        $(".projects").fadeIn();}, 500);
+      $(".home").fadeOut();
+      $(".timeline").fadeOut();
+      $(".skills").fadeOut();
+      $(".cv").fadeOut();
+    });
+    $("#button-cv").click(function () {
+      setTimeout(function() {
+        $(".cv").fadeIn();}, 500);
+      $(".home").fadeOut();
+      $(".timeline").fadeOut();
+      $(".skills").fadeOut();
+      $(".projects").fadeOut();
+    });
+    $("#button-home").click(function () {
+      setTimeout(function() {
+        $(".home").fadeIn();}, 500);
+      $(".timeline").fadeOut();
+      $(".skills").fadeOut();
+      $(".projects").fadeOut();
+      $(".cv").fadeOut();
+    });
+    
+  });
+  

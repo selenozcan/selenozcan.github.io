c20b8e69c7f9a54285de92de2930ade874cca9e4
selenozcan <ozcsel.16@gmail.com>
2022-01-16 00:53:02 +0300

    projects 100% skills 90% with some known bugs

--- selen.js
+++ selen.js
@@ -25,14 +25,14 @@ $(function () {
       $(".skills").fadeOut();
       $(".cv").fadeOut();
     });
-    $("#button-cv").click(function () {
-      setTimeout(function() {
-        $(".cv").fadeIn();}, 500);
-      $(".home").fadeOut();
-      $(".timeline").fadeOut();
-      $(".skills").fadeOut();
-      $(".projects").fadeOut();
-    });
+    // $("#button-cv").click(function () {
+    //   setTimeout(function() {
+    //     $(".cv").fadeIn();}, 500);
+    //   $(".home").fadeOut();
+    //   $(".timeline").fadeOut();
+    //   $(".skills").fadeOut();
+    //   $(".projects").fadeOut();
+    // });
     $("#button-home").click(function () {
       setTimeout(function() {
         $(".home").fadeIn();}, 500);
@@ -42,8 +42,22 @@ $(function () {
       $(".cv").fadeOut();
     });
     
-    var pythonTooltip = "Line 1.&#xa;Line 2";
+    var pythonTooltip = "Python&#xa;&#xa;Experience with Django,&#xa;Flask, Pandas, Tensorflow,&#xa;BeautifulSoup";
     $(".python").attr("tooltip", $.parseHTML(pythonTooltip)[0].data);
+    var javaTooltip = "Java";
+    $(".java").attr("tooltip", $.parseHTML(javaTooltip)[0].data);
+    var cplusplusTooltip = "C++";
+    $(".cplusplus").attr("tooltip", $.parseHTML(cplusplusTooltip)[0].data);
+    var cTooltip = "C";
+    $(".c").attr("tooltip", $.parseHTML(cTooltip)[0].data);
+    var csharpTooltip = "C#"
+    $(".csharp").attr("tooltip", $.parseHTML(csharpTooltip)[0].data);
+    var html5Tooltip = "HTML"
+    $(".html5").attr("tooltip", $.parseHTML(html5Tooltip)[0].data);
+    var css3Tooltip = "CSS"
+    $(".css").attr("tooltip", $.parseHTML(css3Tooltip)[0].data);
+    var javascriptTooltip = "Javascript"
+    $(".javascript").attr("tooltip", $.parseHTML(javascriptTooltip)[0].data);
   });
 
 
